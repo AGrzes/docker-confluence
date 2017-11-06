216b407481d331cda3da56efaca1090ec53b9fd3
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-06 23:03:25 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name confluence -p 8090:8090 --rm --restart=unless-stopped -d confluence
+docker run --name confluence -p 8090:8090 -v /var/atlassian/application-data/confluence:/var/atlassian/application-data/confluence --restart=unless-stopped -d confluence
